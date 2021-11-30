f995c88d39cc7c3d0518b14624d880e0f2e805cc
Gurvan <gurvan22300@gmail.com>
2021-11-30 15:53:01 +0100

    Remove useless service

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 
 const games = require('./games/games.service.js');
-const players = require('./players/players.service.js');
 module.exports = function (app) {
   app.configure(games);
-  app.configure(players);
 };
